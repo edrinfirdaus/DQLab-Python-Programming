1d1c3e35992130ada003d624559b12cbbdda6e45
Edrin Firdaus <93240550+edrinfirdaus@users.noreply.github.com>
2025-05-10 20:55:54 +0700

    Add files via upload

---
+++ Kelas Persiapan/Introduction to Data Science with Python/Dasar-dasar Python/Kodingan/Comments di Python.py
@@ -0,0 +1,27 @@
+# Selanjutnya, aku mempelajari penggunaan comments pada Python.
+
+
+# Comments adalah sekumpulan teks yang dituliskan dalam sebuah program dan tidak akan mempengaruhi hasil dari sebuah program. Berikut adalah contoh penulisan single line comment & multi line comment pada python. 
+
+# # untuk menulis single line comment, atau 
+# '''
+# komentar ini
+# lebih dari 1 baris
+# ''' 
+# untuk menulis multi line comment. 
+
+# Tugas:
+# Ketikkan pada Code Editor contoh single line comment & multi line comment mengikuti contoh di bawah.
+
+
+
+
+# "Walaupun tidak mempengaruhi hasil program, comments adalah salah satu komponen yang penting dalam pengembangan program. Hal tersebut dikarenakan comments dapat kita selipkan di antara sekumpulan statements untuk dapat berkomunikasi dengan rekan data lainnya dalam satu tim.” Senja kembali menjelaskan.
+
+# perintah pada baris ini tidak mempengaruhi program
+...
+#perintah ini tidak akan dieksekusi oleh Python
+#dan perintah ini juga tidak akan dieksekusi
+#perintah ini juga tidak akan dieksekusi
+...
+print("jadi # digunakan untuk membuat comment pada Python")
---
+++ Kelas Persiapan/Introduction to Data Science with Python/Dasar-dasar Python/Kodingan/Python “Hello World” & Statement.py
@@ -0,0 +1,17 @@
+# Senja memberikan aku tugas untuk mulai mempraktikkan struktur bahasa python:
+
+
+# 1. Gantilah tanda '___' dengan mengetikkan contoh potongan code di bawah pada code editor. Pastikan kamu memperhatikan penggunaan huruf besar/kecil, dan tanda titik (.)
+
+
+
+# Note:
+# - Kata "Hello World." & "Saya Aksara, baru belajar Python." disebut dengan statement.
+# - print() adalah fungsi dasar python untuk mencetak tulisan yang diberikan
+ 
+
+# 2. Setelah selesai mengetik syntax, tekan tombol 
+# Kamu akan melihat console pada Code Editor menampilkan output 
+
+print("Hello World.")
+print("Saya Aksara, baru belajar Python.")
