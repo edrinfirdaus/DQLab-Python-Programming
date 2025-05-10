b4131a16b5139a6b3fecb0156c6ec73cc242fc35
Edrin Firdaus <93240550+edrinfirdaus@users.noreply.github.com>
2025-05-10 21:02:35 +0700

    Add files via upload

---
+++ Kelas Persiapan/Introduction to Data Science with Python/Lanjutan Dasar-dasar Python/Kodingan/Menggunakan Library di Python.py
@@ -0,0 +1,24 @@
+# “Senja, satu lagi, boleh ajarin bagaimana menggunakan library di Python? Berhubung untuk data science sangat diperlukan.”
+
+# “Boleh. Library pada Python dapat diakses di kode program dengan mengimportnya terlebih dahulu, ada dua cara yang dapat digunakan. Sebentar saya share ke kamu ya, ini caranya.”
+
+
+# Tugas:
+# Menggunakan perintah import nama_library atau import nama_library as alias.
+
+# Contoh yang umum untuk penggunaan ini adalah:
+
+
+
+ 
+
+# Aku langsung antusias mencoba contoh yang diberikan Senja menggunakan Live Code Editor dengan menekan tombol Run untuk menjalankan kode. Jika pada console tidak ada error yang ditampilkan, berarti aku telah sukses memanggil library pada Python.
+
+
+
+# “Yeay, makasih Senja!” Ini baru jalan awal aku memahami lebih dalam dunia data melalui Python, tapi aku sudah merasa kalau ini bidang yang penuh tantangan. Enggak masalah, karena aku yakin aku bisa!
+
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
---
+++ Kelas Persiapan/Introduction to Data Science with Python/Lanjutan Dasar-dasar Python/Kodingan/Tugas Praktek.py
@@ -0,0 +1,20 @@
+# Sekarang Senja memberikanku tugas baru menggunakan potongan teks berikut:
+
+
+
+ 
+# Tugas:
+# Dengan teks yang diberikan, aku diminta untuk mengetik perintah berikut pada live code editor DQLab
+
+
+
+# 1. Pada baris 2, perintah list akan mengubah text menjadi tipe data list dan mencetak nilainya.
+
+# 2. Pada baris 3, perintah tuple akan mengubah text menjadi tipe data tuple dan mencetak nilainya.
+
+# 3. Pada baris 4, perintah set akan mengubah text menjadi tipe data set dan mencetak nilainya.
+
+text = "Belajar Python di DQLab."
+print(list(text))
+print(tuple(text))
+print(set(text))
