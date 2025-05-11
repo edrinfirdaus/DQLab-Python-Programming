d77bae64cd641b081bb479da8c3cb880ef575ce1
Edrin Firdaus <93240550+edrinfirdaus@users.noreply.github.com>
2025-05-11 19:22:12 +0700

    Add files via upload

---
+++ Kelas Fundamental/Python for Data Professional Beginner - Part 1/Mari Mengenal Python/Kodingan/Lebih Jauh dengan Print.py
@@ -0,0 +1,11 @@
+# Keren! Aku baru tahu kalau Python memiliki fungsi print() yang berguna untuk menampilkan pesan ke layar pengguna.
+
+# Tugas Praktek
+# Sekarang aku diberikan tugas dari Senja untuk menampilkan dua kalimat, yaitu "Halo Dunia" dan "Riset Bahasa Python" menggunakan fungsi print().
+
+# Catatan: Untuk membantu aku, di dalam bagian code editor sudah diberikan template code jawaban dimana aku harus mengganti ___ dengan teks yang diminta oleh soal dari tugas praktek di atas.
+
+# Klik tombol Run untuk menjalankan code tersebut. Dan jika sudah berjalan lancar, tekan Submit untuk mengirim jawaban ke dalam sistem.
+
+print( "Halo Dunia" )
+print("Riset Bahasa Python")
---
+++ Kelas Fundamental/Python for Data Professional Beginner - Part 1/Mari Mengenal Python/Kodingan/Program pertama Hello World.py
@@ -0,0 +1,27 @@
+# Setelah memahami teori-teori fundamental terkait dengan "Apa dan Mengapa Python?", aku diminta membuat sebuah program sederhana dengan Python.
+
+# Fungsi Python yang akan pertama kali aku pelajari adalah fungsi print(). Dengan fungsi print(), aku dapat menampilkan atau mencetak teks ke layar pengguna.
+
+# Syntax atau struktur penggunaan dari fungsi print untuk teks adalah sebagai berikut:
+
+# print(teks)
+# Dimana teks adalah kata atau kalimat bebas yang diapit oleh tanda kutip tunggal (') atau tanda kutip ganda (").
+
+# Beberapa contoh penulisan teks adalah sebagai berikut:
+
+# "DQLab"
+# 'Hai'
+# "Ini adalah teks"
+# "DQLab adalah online learning berbasis Artificial Intelligence"
+# Perhatikan kalau semua contoh teks diapit oleh tanda kutip ganda ataupun tanda kutip tunggal.
+
+# Tugas Praktek
+# Aku diberikan tugas oleh Senja untuk menampilkan pesan "Hello World!" pada layar dengan fungsi print().
+
+# Ketikkan perintah tersebut pada bagian Code Editor seperti ditunjukkan pada screenshot di bawah ini, setelah itu tekan tombol Run.
+
+
+
+# Jika dijalankan dengan benar maka teks "Hello World!" akan muncul di bagian Console. Klik tombol Submit untuk melanjutkan
+
+print("Hello World!")
